69e2dd51f36b4d4025d2854905b1690375dadfbb
Chris Oliver <blastoff@Chriss-MacBook-Pro.local>
2022-08-01 11:01:31 -0700

    fix(): remove firebase log

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,7 +3,6 @@ import {useFirebaseApp } from 'reactfire';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-console.log(9999, firebase)
 export default function Login() {
   const app = useFirebaseApp();
   const auth = getAuth(app);
@@ -12,7 +11,7 @@ export default function Login() {
     // Popup signin flow rather than redirect flow.
     signInFlow: 'popup',
     // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: '/signedIn',
+    signInSuccessUrl: '/',
     // We will display Google and Facebook as auth providers.
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
