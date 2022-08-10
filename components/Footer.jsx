86f33dda96cc2a31b400eddc88c5812f7f76d798
May Ng <mayng@Mays-MacBook-Pro.local>
2022-08-09 20:14:20 -0700

    added placeholders for footer links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,32 @@
 import classNames from "classnames/bind";
 import styles from "../layouts/defaultLayout.module.scss";
+import SocialMediaIcons from "./SocialMediaIcons";
 
 const cx = classNames.bind(styles);
+const dummySocialMedia = [
+  { type: "Twitter", link: "http://www.twitter.com" },
+  { type: "Instagram", link: "http://www.Instagram.com" },
+  { type: "LinkedIn", link: "http://www.LinkedIn.com" },
+];
+
 const Footer = () => {
-  return <section className={cx("footer")}>Footer</section>;
+  return (
+    <section className={cx("footer")}>
+      <div className={cx("footer-links")}>
+        <ul className={cx("link-col")}>
+          <li>Accessibility</li>
+          <li>Privacy Policy</li>
+          <li>Code of Conduct</li>
+          <li>FAQ</li>
+        </ul>
+        <ul className={cx("link-col")}>
+          <li>Contact Us</li>
+          <li>Site Feedback</li>
+        </ul>
+      </div>
+      <SocialMediaIcons socials={dummySocialMedia} />
+    </section>
+  );
 };
 
 export default Footer;
