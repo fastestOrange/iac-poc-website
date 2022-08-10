1ccbfcb8a4e25966deb92ab9a93cc884683c2ab0
May Ng <mayng@Mays-MacBook-Pro.local>
2022-08-10 09:48:22 -0700

    event card updates

--- components/cards/EventWelcomeCard.jsx
+++ components/cards/EventWelcomeCard.jsx
@@ -8,9 +8,8 @@ const cx = classNames.bind(styles);
 const EventWelcomeCard = ({ event }) => {
   //TODO: replace with dynamic props
   return (
-    <div className={cx("card")}>
+    <div className={cx("event-card")}>
       <p className={cx("title")}>Welcome to IAC22</p>
-      <p>Header/Title</p>
       <p className={cx("description")}>
         Some type of specific introduction and explanation on what the following
         events are for. Example; Here you will find events on blank, they are
